c5b24046cb3e1623f75bdd19318965b5fed50710
Eya <veatrizcruz3@gmail.com>
2025-05-23 12:30:45 +0800

    Log in and sign up

--- LoginPage.js
+++ LoginPage.js
@@ -1,22 +1,17 @@
 function SignUp() {
-    window.location.href = "Certificate/Certificate.html";
-}
-function LogIn() {
-    window.location.href = "Certificate/Certificate.html";
-}
-
-function toggleForms() {
-    const signupBox = document.getElementById('signupBox');
-    const loginBox = document.getElementById('loginBox');
-
-    if (loginBox.style.display === 'none') {
-      loginBox.style.display = 'block';
-      signupBox.style.display = 'none';
-    } else {
-      loginBox.style.display = 'none';
-      signupBox.style.display = 'block';
+      window.location.href = "Certificate/Certificate.html";
     }
-  }
 
+    function login() {
+      window.location.href = "Certificate/Certificate.html";
+    }
 
-  
+    function showLogin() {
+      document.getElementById('signupForm').style.display = 'none';
+      document.getElementById('loginForm').style.display = 'flex';
+    }
+
+    function showSignup() {
+      document.getElementById('loginForm').style.display = 'none';
+      document.getElementById('signupForm').style.display = 'flex';
+    }
