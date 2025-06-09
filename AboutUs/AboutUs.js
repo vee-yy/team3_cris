dbc90893e89ea0009e5fb31e719fc72af933d53d
Carl Angelo Lumagui <carllumagui16@gmail.com>
2025-06-09 21:27:58 +0800

    about us

---
+++ AboutUs/AboutUs.js
@@ -0,0 +1,5 @@
+function Home() {
+  window.location.href = '../Certification/Certificate.html';
+}
+
+document.getElementById('year').textContent = new Date().getFullYear();
--- Certification/Certificate.php
+++ Certification/Certificate.php
@@ -30,10 +30,10 @@ $result_full = $stmt->get_result();
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="stylesheet" href="Certificate.css">
 </head>
 
@@ -77,7 +77,6 @@ $result_full = $stmt->get_result();
       </div>
     </div>
   </div>
-
   <!-- Navigation Section -->
   <div class="header-right">
     <nav class="nav-links">
@@ -92,6 +91,7 @@ $result_full = $stmt->get_result();
       </div>
     </nav>
   </div>
+
 </header>
 
 <div class="title">
